2411fbbaa972f1fcbd9f61b344acfe794b97a3d9
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2025-04-10 14:57:29 +0100

    Darkmode/lightmode Toggle Animation Bugfix

--- darkmode.js
+++ darkmode.js
@@ -4,13 +4,13 @@ const themeSwitch = document.getElementById("themeSwitch")
 const enableDarkmode = () => {
     document.body.classList.add("darkmode")
     localStorage.setItem("darkmode", "active")
-    themeSwitch.style.transform = "translateX(calc(50% + 0.03vw))";
+    themeSwitch.style.transform = "translateX(calc(100% + 0.03vw))";
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove("darkmode")
     localStorage.setItem("darkmode", null)
-    themeSwitch.style.transform = "translateX(calc(-50% - 0.03vw))";
+    themeSwitch.style.transform = "translateX(calc(-100% - 0.03vw))";
 }
 
 if (darkmode === "active") {
