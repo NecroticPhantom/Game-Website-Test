20c4be12f1ced2c9f3387b71a3fff12d45ba6cef
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2025-04-10 13:35:09 +0100

    Darkmode Switch Functionallity Added (+ Darkmode Switch CSS Changes)

--- darkmode.js
+++ darkmode.js
@@ -4,11 +4,13 @@ const themeSwitch = document.getElementById("themeSwitch")
 const enableDarkmode = () => {
     document.body.classList.add("darkmode")
     localStorage.setItem("darkmode", "active")
+    themeSwitch.style.transform = "translateX(3.7vw)";
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove("darkmode")
     localStorage.setItem("darkmode", null)
+    themeSwitch.style.transform = "translateX(-0.3vw)";
 }
 
 if (darkmode === "active") {
