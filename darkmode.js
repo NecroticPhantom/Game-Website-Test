0d5c66c02e557b127e3014bfa89057f02ad2e8aa
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2025-04-12 14:36:41 +0100

    CSS units change (vw to rem)

--- darkmode.js
+++ darkmode.js
@@ -1,23 +1,23 @@
-let darkmode = localStorage.getItem("darkmode")
-const themeSwitch = document.getElementById("themeSwitch")
+const themeSwitch = document.getElementById("themeSwitch");
 
 const enableDarkmode = () => {
-    document.body.classList.add("darkmode")
-    localStorage.setItem("darkmode", "active")
-    themeSwitch.style.transform = "translateX(2vw)";
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+  themeSwitch.style.transform = "translateX(2rem)";
+};
 
 const disableDarkmode = () => {
-    document.body.classList.remove("darkmode")
-    localStorage.setItem("darkmode", null)
-    themeSwitch.style.transform = "translateX(-2vw)";
-}
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", "inactive");
+  themeSwitch.style.transform = "translateX(-2rem)";
+};
 
-if (darkmode === "active") {
-    enableDarkmode()
+if (localStorage.getItem("darkmode") === "active") {
+  enableDarkmode();
 }
 
 themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem("darkmode")
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+  localStorage.getItem("darkmode") === "active"
+    ? disableDarkmode()
+    : enableDarkmode();
+});
