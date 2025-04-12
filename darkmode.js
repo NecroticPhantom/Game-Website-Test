23ccfeef31261926f667a1ada4760589c86bdacd
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2025-04-12 15:08:31 +0100

    Minor themeSwitch adjustment

--- darkmode.js
+++ darkmode.js
@@ -3,18 +3,21 @@ const themeSwitch = document.getElementById("themeSwitch");
 const enableDarkmode = () => {
   document.body.classList.add("darkmode");
   localStorage.setItem("darkmode", "active");
-  themeSwitch.style.transform = "translateX(2rem)";
+  themeSwitch.style.left = "4rem";
 };
 
 const disableDarkmode = () => {
   document.body.classList.remove("darkmode");
   localStorage.setItem("darkmode", "inactive");
-  themeSwitch.style.transform = "translateX(-2rem)";
+  themeSwitch.style.left = "0.2rem";
 };
 
 if (localStorage.getItem("darkmode") === "active") {
   enableDarkmode();
 }
+else {
+  disableDarkmode();
+};
 
 themeSwitch.addEventListener("click", () => {
   localStorage.getItem("darkmode") === "active"
